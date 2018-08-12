d96d6687cadb49519ca877b28a5c1f865b172e44
HaoDH <haodh95@gmail.com>
2018-08-12 17:39:35 +0700

    All file

---
+++ js/index.js
@@ -0,0 +1,74 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* global firebase, consile */
+
+var db = firebase.firestore();
+var btn = document.getElementById("btn2");
+
+// db.collection("Post")
+//     .get()
+//     .then(function(querySnapshot) {
+//         querySnapshot.forEach(function(doc) {
+//             console.log(doc.id, " => ", doc.data(), "==>", querySnapshot.size);
+//         });
+//         size = querySnapshot.size;
+//         var source = $("#load-post").html();
+//         var template = Handlebars.compile(source);
+//         var context = {
+//             context: size
+//         }
+//         var el_html = template(context);
+//         $("#divtotalpost").html(el_html);
+//     })
+//     .catch(function(error) {
+//         console.log("Error getting documents: ", error);
+//     });
+
+db.collection("Post")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+        });
+        var size = querySnapshot.size;
+        var context = document.getElementById("totalpost");
+        context.innerText = size;
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+
+db.collection("User")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+        });
+        var size = querySnapshot.size;
+        var context = document.getElementById("totaluser");
+        context.innerText = size;
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+function addData() {
+    var paren = document.getElementById("totalpost");
+    paren.innerText = size;
+}
+
+// $(document).ready(function() {
+//     var source = $("#load-post").html();
+//     var template = Handlebars.compile(source);
+//     var context = {
+//         context: size
+//     }
+//     var el_html = template(context);
+//     $("#divtotalpost").html(el_html);
+//     window.alert("2");
+// })
---
+++ js/login.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function login() {
+    var userEmail = document.getElementById("exampleInputEmail1").value;
+    var userPassword = document.getElementById("exampleInputPassword1").value;
+    window.alert(userEmail + " " + userPassword);
+
+}
